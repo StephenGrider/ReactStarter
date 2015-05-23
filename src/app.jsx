b51f3d04cf53f341ce7c567cae8f58092dff3c2d
stephen grider <ste.grider@gmail.com>
2015-05-23 10:14:14 -0700

    hello world

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,12 @@
 var React = require('react');
-var ThumbnailList = require('./thumbnail-list');
 
-var options = {
-  thumbnailData:  [{
-    title: 'Show Courses',
-    number: 120,
-    header: 'Learn React',
-    description: 'React is a fantastic new front end library for rendering web pages. React is a fantastic new front end library for rendering web pages.',
-    imageUrl: 'https://raw.githubusercontent.com/wiki/facebook/react/react-logo-1000-transparent.png'
-  },{
-    title: 'Show Courses',
-    number: 25,
-    header: 'Learn Gulp',
-    description: 'Gulp will speed up your development workflow.  Gulp will speed up your development workflow.  Gulp will speed up your development workflow.',
-    imageUrl: 'http://brunch.io/images/others/gulp.png'
-  }]
-};
+var Hello = React.createClass({
+  render: function() {
+    return <h1>
+      Hello, React!
+    </h1>
+  }
+});
 
-
-var element = React.createElement(ThumbnailList, options);
+var element = React.createElement(Hello, {});
 React.render(element, document.querySelector('.container'));
--- src/badge.jsx
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-module.exports = React.createClass({
-  render: function() {
-    return <button className="btn btn-primary" type="button">
-      {this.props.title} <span className="badge">{this.props.number}</span>
-    </button>
-  }
-});
